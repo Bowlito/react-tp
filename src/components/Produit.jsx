50925c3a9e50dbc736951895e46b893cb26b6413
Bowlito <julien.catoio@gmail.com>
2025-09-23 09:46:43 +0200

    disabling buttons for primeur

--- src/components/Produit.jsx
+++ src/components/Produit.jsx
@@ -5,8 +5,11 @@ export default function Produit({ produit, setQuantity }) {
     let quantite = useRef(1)
     //let [disabled, setDisabled] = useState(true)
 
+    let [disabled, setDisabled] = useState(false)
+
     function envoyer(){
         setQuantity(quantite.current.value)
+        setDisabled(true)
     }
 
     // function disable(){
@@ -16,7 +19,7 @@ export default function Produit({ produit, setQuantity }) {
         <>
             {produit.nom}, {produit.prix}€, {produit.quantite} pièces, 
             <input type="number" ref={quantite} placeholder="quantite" />
-            <button onClick={envoyer}>Ajouter</button>
+            <button onClick={envoyer} disabled={disabled}>Ajouter</button>
         </>
     )
 
